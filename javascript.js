a13f57df9bde703228ea0c8725ae772b9167e3c4
Carlos Costa <carloscosta@MacBook-Pro.local>
2023-04-18 15:07:21 -0400

    Added the questions, choices, and answers to the quiz in Javascript

--- javascript.js
+++ javascript.js
@@ -1,5 +1,26 @@
 // Define quiz questions and answers
-
+const questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Paris", "Berlin", "London", "Madrid"],
+      answer: "Paris"
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      choices: ["Mars", "Jupiter", "Saturn", "Neptune"],
+      answer: "Jupiter"
+    },
+    {
+      question: "What is the powerhouse of the cell?",
+      choices: ["Nucleus", "Mitochondria", "Ribosome", "Endoplasmic reticulum"],
+      answer: "Mitochondria"
+    },
+    {
+      question: "What does HTML stand for?",
+      choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Tool Markup Language"],
+      answer: "Hyper Text Markup Language"
+    }
+];
 
 // Set up variables and DOM elements
 
