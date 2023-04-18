245e6d40199ecadd557e4e5db1ad125abaf2bc39
Carlos Costa <carloscosta@MacBook-Pro.local>
2023-04-18 15:03:54 -0400

    Added Pseudo code to the javascript file

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// Define quiz questions and answers
+
+
+// Set up variables and DOM elements
+
+
+// Add event listener to start button
+
+
+// Define startQuiz function
+
+
+// Define showQuestion function
+
+
+// Define handleChoice function
+
+
+// Define updateTimer function
+
+
+// Define endQuiz function
+
+
+// Add event listener to submit button
+
+
+// Define saveHighScore function
